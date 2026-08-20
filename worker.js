7c79f17d0508518eab1d041c95ecd4215cd11e0e
musubi202607 <musubi.202607@gmail.com>
2026-08-20 11:16:17 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,1 +1,14 @@
+export default {
+  async fetch(request, env, ctx) {
 
+    return new Response(
+      "musubi-next Worker test OK",
+      {
+        headers: {
+          "content-type": "text/plain;charset=UTF-8"
+        }
+      }
+    );
+
+  }
+};
